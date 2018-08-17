e92c5eaf96650d1e2793983dd98a4f07e2ec0f58
qronix <qroniix@gmail.com>
2018-08-16 21:29:32 -0400

    Virus is spreading, basic interface setup, terrible colors chosen!

--- cell.js
+++ cell.js
@@ -1,13 +1,19 @@
 class Cell{
     constructor(cellObj){
-
+        this.health     = cellObj.health;
+        this.name       = cellObj.name;
+        this.resistance = cellObj.resistance;
+        this.status     = cellObj.status;
+        this.infected   = cellObj.infected;
+        this.cellId     = cellObj.cellId;
     }
 
-    static generateCell(){
+    static generateCell(iD){
         let health = Math.floor(Math.random()*(100-50)+50);
         let resistance = Math.floor(Math.random()*10);
         let status = 'unknown';
         let infected = false;
+        let cellId = iD;
 
         if(health>=80){
             status = 'healthy';
@@ -23,8 +29,27 @@ class Cell{
             health,
             resistance,
             status,
-            infected
+            infected,
+            cellId
         }
         return (returnObj);
+    }
+
+    getInfo(){
+        let cellInfo = {
+            name: this.name,
+            health: this.health,
+            resistance: this.resistance,
+            status: this.status,
+            infected: this.infected
+        }
+        return cellInfo;
+    }
+
+    setStatus(status){
+        this.status = status;
+    }
+    setInfected(infected){
+        this.infected = infected;
     }
 }
--- systemManager.js
+++ systemManager.js
@@ -5,16 +5,16 @@ class systemManager{
     }
     setupCellGroup(){
         for(let i=0; i<this.cellGroupSize; i++){
-            let cellObj = Cell.generateCell();
-            cellObj.name = `Cell: ${i+1}`;
-            this.cellGroup.push(cellObj);
+            let cellObj = Cell.generateCell(i);
+            cellObj.name = `Cell ${i+1}`;
+            this.cellGroup.push(new Cell(cellObj));
         }
     }
     addVirus(virusObj){
         let virus = new virus;
     }
     getCellInfo(cellId){
-        return this.cellGroup[cellId];
+        return this.cellGroup[cellId].getInfo();
     }
     displayCellInfo(cellElement,cellId){
         let cellObj = this.getCellInfo(cellId);
@@ -22,7 +22,7 @@ class systemManager{
         infoWindow.classList.add('infoWindow');
         for(let property in cellObj){
             let infoLine = document.createElement('p');
-            infoLine.innerText = `${property}: ${cellObj[property]}`;
+            infoLine.innerText = `${property[0].toUpperCase() + property.substr(1,property.length)}: ${cellObj[property]}`;
             infoWindow.appendChild(infoLine);
         }
         cellElement.appendChild(infoWindow);
@@ -30,5 +30,49 @@ class systemManager{
     removeCellInfo(cellElement){
         let target = cellElement.childNodes[0];
         cellElement.removeChild(target);
+    }
+    begin(){
+        let virusCellStart = Math.floor(Math.random()*this.cellGroup.length-1);
+        this.cellGroup[virusCellStart].setInfected(true);
+        this.updateSystem();
+    }
+     updateSystem(){
+        var id = setInterval(()=>{
+            this.cellGroup.forEach((cell)=>{
+                this.updateCell(cell);
+            });
+        },100);
+    }
+    //implement async await with promises here to make for a smoother infection
+    updateCell(cell){
+        if(cell.infected){
+
+            //check if surrounding cells can be infected
+            this.updateCellDisplay(cell.cellId);
+            let eligibleCells = [];
+            let surroundingCells = {
+                topLeft:     (cell.cellId - 9),
+                above:       (cell.cellId - 8),
+                topRight:    (cell.cellId - 7),
+                right:       (cell.cellId + 1),
+                bottomRight: (cell.cellId + 9),
+                bottom:      (cell.cellId + 8),
+                bottomLeft:  (cell.cellId + 7),
+                left:        (cell.cellId - 1)
+            }
+            for(let nearCell in surroundingCells){
+                if(this.cellGroup[surroundingCells[nearCell]] !== undefined){
+                    eligibleCells.push(surroundingCells[nearCell]);
+                }
+            }
+            let target = Math.floor(Math.random()*(eligibleCells.length));
+            let targetId = eligibleCells[target];
+            this.cellGroup[targetId].setInfected(true);
+        }
+    }
+
+    updateCellDisplay(cellId){
+        let target = document.getElementById(cellId);
+        target.classList.add('infected');
     }
 }
